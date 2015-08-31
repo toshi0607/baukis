6ecc2ca2da439d4936b5f0122835b8a0a78fe005
Tshinori Sugita <sugita@c-fo.com>
2015-09-01 01:43:25 +0900

    handle 404 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
   rescue_from Exception, with: :rescue500
   rescue_from Forbidden, with: :rescue403
   rescue_from IpAddressRejected, with: :rescue403
+  rescue_from ActionController::RoutingError, with: :rescue404
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
 
   private
   def set_layout
@@ -24,6 +26,11 @@ class ApplicationController < ActionController::Base
   def rescue403(e)
     @exception = e
     render 'errors/forbidden', status: 403
+  end
+
+  def rescue404(e)
+    @exception = e
+    render 'errors/not_found', status: 404
   end
 
   def rescue500(e)
