35f572e2901a02b58b3454bfeb7ce53be4422ce9
Tshinori Sugita <sugita@c-fo.com>
2015-09-02 01:45:17 +0900

    add spec for admin model

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Administrator, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#password=' do
+    example '文字列を与えると、hashed_passwordは長さ60の文字列になる' do
+      admin = Administrator.new
+      admin.password = 'baukis'
+      expect(admin.hashed_password).to be_kind_of(String)
+      expect(admin.hashed_password.size).to eq(60)
+    end
+
+    example 'nilを与えると、hashed_passwordはnilになる' do
+      admin = Administrator.new(hashed_password: 'x')
+      admin.password = nil
+      expect(admin.hashed_password).to be_nil
+    end
+  end
 end
