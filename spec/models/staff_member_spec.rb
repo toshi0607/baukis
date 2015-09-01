d713094921505641d23651a58cb737ef00deeaf5
Tshinori Sugita <sugita@c-fo.com>
2015-09-02 00:57:40 +0900

    add test case

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe StaffMember, :type => :model do
       expect(member.hashed_password).to be_kind_of(String)
       expect(member.hashed_password.size).to eq(60)
     end
+
+    example 'nilを与えると、hashed_passwordはnilになる' do
+      member = StaffMember.new(hashed_password: 'x')
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
   end
 end
