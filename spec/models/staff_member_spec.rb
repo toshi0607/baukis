83a9b3c71835c6b08053b0b93cf113b9c13342a2
Tshinori Sugita <sugita@c-fo.com>
2015-09-02 00:55:46 +0900

    add encryption

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe StaffMember, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#password=' do
+    example '文字列を与えると、hashed_passwordは長さ60の文字列になる' do
+      member = StaffMember.new
+      member.password = 'baukis'
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+  end
 end
